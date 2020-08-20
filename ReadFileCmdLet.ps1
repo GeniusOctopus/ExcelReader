d682563b6b65167afc3bd5750880155490a7ee4d
GeniusOctopus <58113292+GeniusOctopus@users.noreply.github.com>
2020-08-20 11:56:25 +0200

    Update ReadFileCmdLet.ps1

--- ReadFileCmdLet.ps1
+++ ReadFileCmdLet.ps1
@@ -1,9 +1,54 @@
-#read the excel file
+<#
+.SYNOPSIS
+    This function reads an excel file.
+
+.DESCRIPTION
+    A databaseconnection gets established and through a SQL statement you query the Excel File.
+    The function returns an object of type System.Data.DataTable
+
+.PARAMETER strProvider
+    The OleDb Provider Microsoft.ACE.OLEDB.12.0
+
+.PARAMETER strDataSource
+    Basically the path to the file
+
+.PARAMETER strExtend
+    The information that it is an Excel File
+
+.PARAMETER strQuery
+    The SQL statement
+
+.EXAMPLE
+    The example below shows how to call the function
+    PS C:\> Get-ExcelFileContent $strProvider $strDataSource $strExtend $strQuery
+
+.Example
+    This example shows example parameters
+    PS C:\> Get-ExcelFileContent "Provider=Microsoft.ACE.OLEDB.12.0" "Data Source = C:\Users\Me\Desktop\example.xlsx "Extended Properties=Excel 8.0" "Select * from [Tabelle1$]"
+
+#>
 function Get-ExcelFileContent
 {
     [OutputType([System.Data.DataTable])]
     [CmdLetBinding()]
-    param()
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $strProvider,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $strDataSource,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $strExtend,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $strQuery
+    )
 
     BEGIN
     {
@@ -19,15 +64,22 @@ function Get-ExcelFileContent
 
         #set the connection for the command
         $sqlCommand.Connection = $objConn
-    }
-    PROCESS
-    {
-        #establish new databaseconnection
-        $objConn.open()
+        
+        try
+        {
+            #establish new databaseconnection
+            $objConn.open()
+        }
+        catch
+        {
+            throw("Es ist ein Fehler aufgetreten. Die Excel Datei konnte nicht geöffnet werden. Bitte überprüfen Sie den Dateipfad und stellen Sie sicher, dass die Datei nciht geöffnet ist.")
+        }
 
         #give some output for the user
         Write-Host("Lese Exceldatei...")
-
+    }
+    PROCESS
+    {
         #instatntiate a new object of type 'System.Data.DataTable' with the variable tablename 'Exceldatei'
         $table = New-Object System.Data.DataTable 'Exceldatei'
 
